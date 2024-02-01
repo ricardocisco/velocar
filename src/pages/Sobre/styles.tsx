8257db5116ee71ca26d17c1fc80ddaaedf3814cc
ricardocisco <ricardopenga70@gmail.com>
2024-02-01 14:01:56 -0300

    update

--- src/pages/Sobre/styles.tsx
+++ src/pages/Sobre/styles.tsx
@@ -7,19 +7,31 @@ export const SobreBox = styled.div`
   justify-content: space-between;
   width: 1400px;
   margin: 0 auto;
+  @media (max-width: 1400px){
+    width: 100%;
+    flex-direction: column;
+  }
 `
 
 export const SobreList = styled.div`
+  margin-top: 25px;
+  flex-direction: column;
   width: 50%;
   & div{
     display: flex;
     align-items: center;
     font-size: ${Theme.font.sizes.xsmall};
   }
+  @media (max-width: 1400px){
+    width: 100%;
+    flex-direction: column;
+  }
 `
 
 export const ListStyled = styled.li`
   list-style: none;
+  font-size: ${Theme.font.sizes.xxxsmall};
+  text-decoration: underline;
 `
 
 export const StyledH = styled.h1`
@@ -32,8 +44,10 @@ export const StyledP = styled.p`
 `
 
 export const SobreText = styled.div`
+  padding: 0 20px 0 20px;
 `
 
 export const SobreWrapper = styled.div`
-
+  margin-top: 25px;
+  margin-bottom: 20px;
 `
